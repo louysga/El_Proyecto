9060e146508d7ddca878173f774ba4e536c65ad2
juani <juani@DESKTOP-908G301>
2022-04-22 21:14:18 -0400

    Agregar archivos de proyecto.

---
+++ El_Proyecto/FmAcerca.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace El_Proyecto
+{
+    partial class FmAcerca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FmAcerca));
+            this.PNFondo = new Telerik.WinControls.UI.RadPanel();
+            this.radPanel2 = new Telerik.WinControls.UI.RadPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.roundRectShape1 = new Telerik.WinControls.RoundRectShape(this.components);
+            this.PNEncuadre1 = new Telerik.WinControls.UI.RadPanel();
+            this.LBPosicion1 = new System.Windows.Forms.Label();
+            this.LBMatricula1 = new System.Windows.Forms.Label();
+            this.LBNombre1 = new System.Windows.Forms.Label();
+            this.PNFoto1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LBTitulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PNFondo)).BeginInit();
+            this.PNFondo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel2)).BeginInit();
+            this.radPanel2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PNEncuadre1)).BeginInit();
+            this.PNEncuadre1.SuspendLayout();
+            this.PNFoto1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PNFondo
+            // 
+            this.PNFondo.AutoSize = true;
+            this.PNFondo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.PNFondo.Controls.Add(this.radPanel2);
+            this.PNFondo.Controls.Add(this.PNEncuadre1);
+            this.PNFondo.Controls.Add(this.LBTitulo);
+            this.PNFondo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PNFondo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PNFondo.Location = new System.Drawing.Point(0, 0);
+            this.PNFondo.Name = "PNFondo";
+            this.PNFondo.Size = new System.Drawing.Size(1030, 490);
+            this.PNFondo.TabIndex = 3;
+            this.PNFondo.Tag = "Usuario:";
+            this.PNFondo.ThemeName = "ControlDefault";
+            ((Telerik.WinControls.Primitives.FillPrimitive)(this.PNFondo.GetChildAt(0).GetChildAt(0))).BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(77)))));
+            ((Telerik.WinControls.Primitives.FillPrimitive)(this.PNFondo.GetChildAt(0).GetChildAt(0))).BackColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(0)))), ((int)(((byte)(85)))));
+            ((Telerik.WinControls.Primitives.FillPrimitive)(this.PNFondo.GetChildAt(0).GetChildAt(0))).BackColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(2)))), ((int)(((byte)(89)))));
+            ((Telerik.WinControls.Primitives.FillPrimitive)(this.PNFondo.GetChildAt(0).GetChildAt(0))).Shape = null;
+            // 
+            // radPanel2
+            // 
+            this.radPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.radPanel2.Controls.Add(this.label4);
+            this.radPanel2.Controls.Add(this.label5);
+            this.radPanel2.Controls.Add(this.label6);
+            this.radPanel2.Controls.Add(this.panel2);
+            this.radPanel2.Location = new System.Drawing.Point(602, 91);
+            this.radPanel2.Name = "radPanel2";
+            this.radPanel2.Size = new System.Drawing.Size(344, 349);
+            this.radPanel2.TabIndex = 5;
+            ((Telerik.WinControls.UI.RadPanelElement)(this.radPanel2.GetChildAt(0))).Shape = this.roundRectShape1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(145, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Lider";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(122, 259);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "2021-1227";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(65, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(210, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Yoquelvis Jorge Abreu";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(77, 31);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(191, 177);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::El_Proyecto.Properties.Resources.Foto_Yoque;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(191, 177);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // PNEncuadre1
+            // 
+            this.PNEncuadre1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.PNEncuadre1.Controls.Add(this.LBPosicion1);
+            this.PNEncuadre1.Controls.Add(this.LBMatricula1);
+            this.PNEncuadre1.Controls.Add(this.LBNombre1);
+            this.PNEncuadre1.Controls.Add(this.PNFoto1);
+            this.PNEncuadre1.Location = new System.Drawing.Point(93, 91);
+            this.PNEncuadre1.Name = "PNEncuadre1";
+            this.PNEncuadre1.Size = new System.Drawing.Size(344, 349);
+            this.PNEncuadre1.TabIndex = 4;
+            ((Telerik.WinControls.UI.RadPanelElement)(this.PNEncuadre1.GetChildAt(0))).Text = "";
+            ((Telerik.WinControls.UI.RadPanelElement)(this.PNEncuadre1.GetChildAt(0))).Shape = this.roundRectShape1;
+            // 
+            // LBPosicion1
+            // 
+            this.LBPosicion1.AutoSize = true;
+            this.LBPosicion1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBPosicion1.ForeColor = System.Drawing.Color.White;
+            this.LBPosicion1.Location = new System.Drawing.Point(131, 293);
+            this.LBPosicion1.Name = "LBPosicion1";
+            this.LBPosicion1.Size = new System.Drawing.Size(88, 25);
+            this.LBPosicion1.TabIndex = 3;
+            this.LBPosicion1.Text = "Co-Lider";
+            // 
+            // LBMatricula1
+            // 
+            this.LBMatricula1.AutoSize = true;
+            this.LBMatricula1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBMatricula1.ForeColor = System.Drawing.Color.White;
+            this.LBMatricula1.Location = new System.Drawing.Point(121, 259);
+            this.LBMatricula1.Name = "LBMatricula1";
+            this.LBMatricula1.Size = new System.Drawing.Size(107, 25);
+            this.LBMatricula1.TabIndex = 2;
+            this.LBMatricula1.Text = "2021-0339";
+            // 
+            // LBNombre1
+            // 
+            this.LBNombre1.AutoSize = true;
+            this.LBNombre1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBNombre1.ForeColor = System.Drawing.Color.White;
+            this.LBNombre1.Location = new System.Drawing.Point(80, 222);
+            this.LBNombre1.Name = "LBNombre1";
+            this.LBNombre1.Size = new System.Drawing.Size(187, 25);
+            this.LBNombre1.TabIndex = 1;
+            this.LBNombre1.Text = "Louys Genao Abreu";
+            // 
+            // PNFoto1
+            // 
+            this.PNFoto1.BackColor = System.Drawing.Color.White;
+            this.PNFoto1.Controls.Add(this.pictureBox1);
+            this.PNFoto1.Location = new System.Drawing.Point(76, 31);
+            this.PNFoto1.Name = "PNFoto1";
+            this.PNFoto1.Size = new System.Drawing.Size(191, 177);
+            this.PNFoto1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::El_Proyecto.Properties.Resources.Foto_Louys;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 177);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LBTitulo
+            // 
+            this.LBTitulo.AutoSize = true;
+            this.LBTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.LBTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBTitulo.ForeColor = System.Drawing.Color.White;
+            this.LBTitulo.Location = new System.Drawing.Point(449, 28);
+            this.LBTitulo.Name = "LBTitulo";
+            this.LBTitulo.Size = new System.Drawing.Size(138, 29);
+            this.LBTitulo.TabIndex = 18;
+            this.LBTitulo.Text = "Integrantes:";
+            // 
+            // FmAcerca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 490);
+            this.Controls.Add(this.PNFondo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FmAcerca";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FmGrupoEntidades";
+            ((System.ComponentModel.ISupportInitialize)(this.PNFondo)).EndInit();
+            this.PNFondo.ResumeLayout(false);
+            this.PNFondo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel2)).EndInit();
+            this.radPanel2.ResumeLayout(false);
+            this.radPanel2.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PNEncuadre1)).EndInit();
+            this.PNEncuadre1.ResumeLayout(false);
+            this.PNEncuadre1.PerformLayout();
+            this.PNFoto1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadPanel PNFondo;
+        private System.Windows.Forms.Label LBTitulo;
+        private Telerik.WinControls.UI.RadPanel radPanel2;
+        private Telerik.WinControls.UI.RadPanel PNEncuadre1;
+        private System.Windows.Forms.Label LBPosicion1;
+        private System.Windows.Forms.Label LBMatricula1;
+        private System.Windows.Forms.Label LBNombre1;
+        private System.Windows.Forms.Panel PNFoto1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Telerik.WinControls.RoundRectShape roundRectShape1;
+    }
+}
---
+++ El_Proyecto/FmInformacion.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace El_Proyecto
+{
+    partial class FmInformacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FmInformacion));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LBTitulo = new System.Windows.Forms.Label();
+            this.PBImagenLogo = new System.Windows.Forms.PictureBox();
+            this.PNFondo = new Telerik.WinControls.UI.RadPanel();
+            this.BTAtras = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PBImagenLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PNFondo)).BeginInit();
+            this.PNFondo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label3.Location = new System.Drawing.Point(90, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(209, 20);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "© Copyright El Grupo 2022";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(151, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 29);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Version:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LBTitulo
+            // 
+            this.LBTitulo.AutoSize = true;
+            this.LBTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.LBTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.LBTitulo.ForeColor = System.Drawing.Color.White;
+            this.LBTitulo.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.LBTitulo.Location = new System.Drawing.Point(104, 162);
+            this.LBTitulo.Name = "LBTitulo";
+            this.LBTitulo.Size = new System.Drawing.Size(198, 29);
+            this.LBTitulo.TabIndex = 18;
+            this.LBTitulo.Text = "Abstract Contacts";
+            // 
+            // PBImagenLogo
+            // 
+            this.PBImagenLogo.BackColor = System.Drawing.Color.Transparent;
+            this.PBImagenLogo.Image = global::El_Proyecto.Properties.Resources.Logo_de_la_aplicacion;
+            this.PBImagenLogo.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.PBImagenLogo.Location = new System.Drawing.Point(156, 49);
+            this.PBImagenLogo.Name = "PBImagenLogo";
+            this.PBImagenLogo.Size = new System.Drawing.Size(95, 95);
+            this.PBImagenLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PBImagenLogo.TabIndex = 2;
+            this.PBImagenLogo.TabStop = false;
+            // 
+            // PNFondo
+            // 
+            this.PNFondo.AutoSize = true;
+            this.PNFondo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.PNFondo.Controls.Add(this.BTAtras);
+            this.PNFondo.Controls.Add(this.label3);
+            this.PNFondo.Controls.Add(this.label2);
+            this.PNFondo.Controls.Add(this.label1);
+            this.PNFondo.Controls.Add(this.LBTitulo);
+            this.PNFondo.Controls.Add(this.PBImagenLogo);
+            this.PNFondo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PNFondo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.PNFondo.Location = new System.Drawing.Point(0, 0);
+            this.PNFondo.Name = "PNFondo";
+            this.PNFondo.Size = new System.Drawing.Size(401, 387);
+            this.PNFondo.TabIndex = 4;
+            this.PNFondo.Tag = "Usuario:";
+            this.PNFondo.ThemeName = "ControlDefault";
+            ((Telerik.WinControls.Primitives.FillPrimitive)(this.PNFondo.GetChildAt(0).GetChildAt(0))).BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(0)))), ((int)(((byte)(77)))));
+            ((Telerik.WinControls.Primitives.FillPrimitive)(this.PNFondo.GetChildAt(0).GetChildAt(0))).BackColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(137)))), ((int)(((byte)(0)))), ((int)(((byte)(85)))));
+            ((Telerik.WinControls.Primitives.FillPrimitive)(this.PNFondo.GetChildAt(0).GetChildAt(0))).BackColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(2)))), ((int)(((byte)(89)))));
+            ((Telerik.WinControls.Primitives.FillPrimitive)(this.PNFondo.GetChildAt(0).GetChildAt(0))).Shape = null;
+            // 
+            // BTAtras
+            // 
+            this.BTAtras.BackColor = System.Drawing.Color.Transparent;
+            this.BTAtras.FlatAppearance.BorderSize = 0;
+            this.BTAtras.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTAtras.Image = global::El_Proyecto.Properties.Resources.Icono_de_otra_x_mas_grande;
+            this.BTAtras.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.BTAtras.Location = new System.Drawing.Point(346, 12);
+            this.BTAtras.Name = "BTAtras";
+            this.BTAtras.Size = new System.Drawing.Size(43, 36);
+            this.BTAtras.TabIndex = 23;
+            this.BTAtras.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BTAtras.UseVisualStyleBackColor = false;
+            this.BTAtras.Click += new System.EventHandler(this.BTAtras_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(134, 246);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 29);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "1.0.0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FmInformacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 387);
+            this.Controls.Add(this.PNFondo);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FmInformacion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FmInformacion";
+            ((System.ComponentModel.ISupportInitialize)(this.PBImagenLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PNFondo)).EndInit();
+            this.PNFondo.ResumeLayout(false);
+            this.PNFondo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LBTitulo;
+        private System.Windows.Forms.PictureBox PBImagenLogo;
+        private Telerik.WinControls.UI.RadPanel PNFondo;
+        private System.Windows.Forms.Button BTAtras;
+        private System.Windows.Forms.Label label2;
+    }
+}
